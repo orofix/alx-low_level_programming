a8e7935110d054d25d2cc1fa905d1f7d1b27c29c
orofix <orofix108@gmail.com>
2023-09-14 09:30:03 -0700

    Read

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -13,4 +13,4 @@ int _isupper(int c)
 	if (c >= 65 && c <= 90)
 		return (1);
 	else
-	
+		return (0)
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_numbers - print a sequence from 0 to 9
  *
- * Return: Always 0.
- */
-int main(void)
+ * automated sequence to variable needed
+ * Return: no returns
+*/
+void print_numbers(void)
 {
-	print_numbers();
-	return (0);
-}
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}}
