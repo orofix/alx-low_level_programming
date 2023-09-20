ff3df7c6ef64d9cae606ccb25a0098c1d6746ba7
orofix <orofix108@gmail.com>
2023-09-20 13:45:26 -0700

    Read

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes and decodes strings
+ * @a: string to upper
+ *
+ * Return: pointer a
+ */
+char *rot13(char *a)
+{
+	int index;
+	int incheck;
+	char comp[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char leet[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	for (index = 0; *(a + index) != '\0'; index++)
+	{
+		for (incheck = 0; *(leet + incheck) != '\0'; incheck++)
+		{
+			if (*(a + index) == *(leet + incheck))
+			{
+				*(a + index) = *(comp + incheck);
+				break;
+			}
+		}
+	}
+	return (a);
+}
