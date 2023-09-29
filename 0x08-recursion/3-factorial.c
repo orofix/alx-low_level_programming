7066b85a9d0ba4bc93c0b73451050c7dd64c69a3
orofix <orofix108@gmail.com>
2023-09-29 11:12:25 -0700

    read

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - gets factorial
+ * @n: the string
+ * Return: returns factorial value
+ */
+
+int factorial(int n)
+{
+	int counter;
+
+	if (n < 0)
+		return (-1);
+	if (n > 0)
+	{
+		counter = (factorial(n - 1));
+		return (counter * n);
+	}
+	else
+		return (1);
+}
